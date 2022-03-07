339e712043df626126d1c11de1d5e7fd21d2e605
Shani Ferbstein <shani_f@malam.com>
2022-03-07 10:17:27 +0200

    Change main 07\03

--- src/main/java/Coin/Main.java
+++ src/main/java/Coin/Main.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.Scanner;
 
+
 public class Main {
     public static void main(String[] args) throws IOException {
         Scanner myObj = new Scanner(System.in);//Setting up a SCANNER
